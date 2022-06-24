bc588a6d5ad1c1c7bf47113d6ad05e6fe18bbf0c
RuminasVT2 <107530906+RuminasVT2@users.noreply.github.com>
2022-06-24 20:06:48 +0800

    Update xp-regist.js

--- plugins/xp-regist.js
+++ plugins/xp-regist.js
@@ -15,8 +15,7 @@ let handler = async function (m, { text, usedPrefix }) {
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
-  m.reply(`
-`----------〔  SUCCEED 〕----------
+  m.reply(`----------〔  SUCCEED 〕----------
 
 ╭─┄┄─❒ INFO
 │ Nama: ${name}
